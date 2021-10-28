33313b662fa92a7b41ce87ac53bff79337374d6a
Montoya Edu <montoya.edu@gmail.com>
2021-10-28 15:53:18 +0200

    Read body and forward request.

--- index.js
+++ index.js
@@ -2,11 +2,25 @@
 
 const http = require('http')
 const httpProxy = require('http-proxy')
+const streamify = require('stream-array')
 
 const proxy = httpProxy.createProxyServer({})
 
-const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://127.0.0.1:9999'})
+const server = http.createServer(async (req, res) => {
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  const data = Buffer.concat(buffers).toString()
+
+  console.log(data)
+
+  proxy.web(req, res, {
+    target: 'http://127.0.0.1:9999',
+    buffer: streamify(buffers)
+  })
 })
 
 server.listen(8000)
